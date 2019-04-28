033be59dc9849b3eb1f18c7487a7eb951456e0af
Nick Daigle <nickdaigle66@gmail.com>
2019-04-28 14:57:42 -0500

    Initial checkin
    Completed Section 1 - Section 2.5

---
+++ RPG Project/Assets/Scripts/Behaviors/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private Ray _lastRay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+
+        Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100);
+
+        var agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+    }
+}
