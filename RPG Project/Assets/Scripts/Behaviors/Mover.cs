e107f91bf4d0dd80ac366cc290945f10d9c4a453
Nick Daigle <nickdaigle66@gmail.com>
2019-04-30 22:10:50 -0500

    Finish 2.12 - 2.14

--- RPG Project/Assets/Scripts/Behaviors/Mover.cs
+++ RPG Project/Assets/Scripts/Behaviors/Mover.cs
@@ -7,19 +7,34 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    private Ray _lastRay;
+    private Camera _mainCamera;
+
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveToCursor();
         }
 
-        Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100);
+        
+    }
 
-        var agent = GetComponent<NavMeshAgent>();
-        agent.destination = target.position;
+    private void MoveToCursor()
+    {
+        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if(hasHit)
+        {
+            var agent = GetComponent<NavMeshAgent>();
+            agent.destination = hit.point;
+        }
     }
 }
